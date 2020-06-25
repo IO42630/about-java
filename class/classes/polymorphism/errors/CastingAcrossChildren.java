8c7882e8df704a29a7637f2c96c0ddaa5918d386
Ivan Olexyn <ivan@olexyn.com>
2020-06-25 23:28:20 +0200

    refactor example

---
+++ class/classes/polymorphism/errors/CastingAcrossChildren.java
@@ -0,0 +1,20 @@
+package classes.polymorphism.errors;
+
+class Parent {}
+
+class ChildA extends Parent {}
+
+class ChildB extends Parent {}
+
+public class CastingAcrossChildren {
+
+    public static void main() {
+        Parent childAasParent = new ChildA();
+        if (childAasParent instanceof ChildB) {
+            ChildB childB = (ChildB) childAasParent; // -> R-Error (ClassCastException)
+        }
+    }
+}
+
+
+
