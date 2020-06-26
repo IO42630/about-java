d304a13ac0bfd65d8f81196dae4661cff7ee33a4
Ivan Olexyn <ivan@olexyn.com>
2020-06-26 23:33:53 +0200

    format

--- stream/src/main/java/com/olexyn/about/java/streams/Streams.java
+++ stream/src/main/java/com/olexyn/about/java/streams/Streams.java
@@ -13,10 +13,7 @@ public class Streams {
 
         Stream<Integer> stream;
         stream = Stream.of(1, 2, 3, 4);
-        stream = Stream.of(new Integer[]{1,
-                                         2,
-                                         3,
-                                         4});
+        stream = Stream.of(new Integer[]{1, 2, 3, 4});
         // stream = Stream.of(new int[]{1,2,3,4}); -> C-Error
 
 
@@ -33,38 +30,34 @@ public class Streams {
         Stream<String> stream1 = Stream.generate(() -> {return "abc";});
         Stream<String> stream2 = Stream.iterate("abc", (i) -> i);
 
-        LongStream is = Arrays.stream(new long[]{1,
-                                                 2,
-                                                 3,
-                                                 4});
+        LongStream is = Arrays.stream(new long[]{1, 2, 3, 4});
         IntStream is2 = "abc".chars();
     }
 
 
-    void collectStream(){
+    void collectStream() {
 
-        Stream<Integer> intStream = Stream.of(1,2,3,4);
+        Stream<Integer> intStream = Stream.of(1, 2, 3, 4);
         List<Integer> intList = intStream.collect(Collectors.toList());
         System.out.println(intList); //prints [1, 2, 3, 4]
 
-        intStream = Stream.of(1,2,3,4); //stream is closed, so we need to create it again
-        Map<Integer,Integer> intMap = intStream.collect(Collectors.toMap(i -> i, i -> i + 10));
+        intStream = Stream.of(1, 2, 3, 4); //stream is closed, so we need to create it again
+        Map<Integer, Integer> intMap = intStream.collect(Collectors.toMap(i -> i, i -> i + 10));
         System.out.println(intMap); //prints {1=11, 2=12, 3=13, 4=14}
 
     }
 
 
-    void modifySteam(){
+    void modifySteam() {
 
         List<Integer> myList = new ArrayList<>();
-        for(int i=0; i<100; i++) myList.add(i);
+        for (int i = 0; i < 100; i++) myList.add(i);
         Stream<Integer> sequentialStream = myList.stream();
 
         Stream<Integer> highNums = sequentialStream.filter(p -> p > 90); //filter numbers greater than 90
         System.out.print("High Nums greater than 90=");
-        highNums.forEach(p -> System.out.print(p+" "));
+        highNums.forEach(p -> System.out.print(p + " "));
 //prints "High Nums greater than 90=91 92 93 94 95 96 97 98 99 "
-
 
 
         Stream<String> names = Stream.of("aBc", "d", "ef");
@@ -83,13 +76,9 @@ public class Streams {
         System.out.println(naturalSorted); //[123456, aBc, d, ef]
 
 
-        Stream<List<String>> namesOriginalList = Stream.of(
-                Arrays.asList("Pankaj"),
-                Arrays.asList("David", "Lisa"),
-                Arrays.asList("Amit"));
+        Stream<List<String>> namesOriginalList = Stream.of(Arrays.asList("Pankaj"), Arrays.asList("David", "Lisa"), Arrays.asList("Amit"));
 //flat the stream from List<String> to String stream
-        Stream<String> flatStream = namesOriginalList
-                .flatMap(strList -> strList.stream());
+        Stream<String> flatStream = namesOriginalList.flatMap(strList -> strList.stream());
 
         flatStream.forEach(System.out::println);
 
