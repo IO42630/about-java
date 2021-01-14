466a19de4dc2a5fa81087717da8019d2f7db59bc
Ivan Olexyn <ivan@olexyn.com>
2021-01-14 05:18:15 +0100

    misc

--- io/src/main/java/com/olexyn/about/java/io/Print.java
+++ io/src/main/java/com/olexyn/about/java/io/Print.java
@@ -1,12 +1,21 @@
 package com.olexyn.about.java.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 
 public class Print {
 
 
-    public static void main(String... args) throws FileNotFoundException {
+    public static void main(String... args) throws IOException {
         PrintWriter out = new PrintWriter("zoo.log");
         out.print(5); // PrintWriter method
         out.write(String.valueOf(5)); // Writer method
@@ -15,6 +24,21 @@ public class Print {
         Object obj = new Object();
         out.print(obj); // PrintWriter method
         out.write(obj==null ? "null": obj.toString()); // Writer method
+
+
+        File source = new File("zoo.log");
+        try (PrintWriter out2 = new PrintWriter(new BufferedWriter(new FileWriter(source)))) {
+            out2.print("Today's weather is: ");
+            out2.println("Sunny");
+            out2.print("Today's temperature at the zoo is: ");
+            out2.print(1/3.0);
+            out2.println('C');
+            out2.format("It has rained 10.12 inches this year");
+            out2.println();
+            out2.printf("It may rain 21.2 more inches this year");
+        }
+
+        Reader in = new BufferedReader(new InputStreamReader(new BufferedInputStream(System.in)));
     }
 
 
