440544f66be117d6ec72155ed9aefa15f4fe0774
iol <ivan.olexyn@elca.ch>
2021-01-19 21:00:39 +0100

    misc nio2

--- io/src/main/java/com/olexyn/about/java/io/NIODemo.java
+++ io/src/main/java/com/olexyn/about/java/io/NIODemo.java
@@ -7,9 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class NIODemo {
+    
+    public static final Path resources = Paths.get(System.getProperty("user.dir") + "/io/src/main/resources");
 
     public static void main(String... args){
-        pathNames();
+        // pathNames();
+        // rootTraversal();
+        conversions();
     }
 
 
@@ -57,5 +61,42 @@ public class NIODemo {
 
 
 
+    }
+    
+    static void rootTraversal() {
+        Path path = Paths.get(resources.toString(), "foo.txt");
+
+        Path foo = path.getFileName(); // foo.txt
+        Path fooRoot = path.getFileName().getRoot(); // always null as this is a relative path
+        Path root = path.getRoot();    // C:\
+        
+        while((path = path.getParent()) != null) {
+            int br = 0;                // C:\Users\iol -> C:\Users -> C:\ -> break
+        }
+    }
+    
+    static void conversions() {
+        resources.getFileName().isAbsolute(); // false
+        resources.getFileName().toAbsolutePath().isAbsolute(); // true
+        
+        Path originalRes = resources;
+        Path newRes = resources.subpath(0, resources.getNameCount());
+        
+        boolean b = originalRes.toString().equals(resources.getRoot().toString() + newRes); // true
+        // subpath ignores root
+        
+        // resources.subpath(0, resources.getNameCount() + 1); // R-Exception
+        //resources.subpath(1,1); // R-Exception
+        int br = 0;
+    }
+    
+    static void relativize() {
+        // returns path of param as relative path as if viewed from current file
+        Path currentPath = resources.getParent();
+        Path otherPath = resources;
+        Path otherRelativeToCurrent = currentPath.relativize(otherPath);
+        
+        // relativize requires both Paths to be either absolute or relative, else R-Exception
+        // in windows also requires same root if absolute
     }
 }
