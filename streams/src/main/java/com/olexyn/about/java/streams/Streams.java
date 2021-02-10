e81411a5f9b99254e4f2c9eafaefb046cf99c3e5
iol <ivan.olexyn@elca.ch>
2021-02-10 13:13:32 +0100

    update streams

--- streams/src/main/java/com/olexyn/about/java/streams/Streams.java
+++ streams/src/main/java/com/olexyn/about/java/streams/Streams.java
@@ -8,6 +8,20 @@ import java.util.stream.Stream;
 
 public class Streams {
 
+    private static List<Integer> integerList = new ArrayList<>();
+    private static List<String> stringList = new ArrayList<>();
+
+    private static Stream<Integer> integerStream;
+
+    static {
+        for (int i = 0; i < 10; i++) integerList.add(i);
+    }
+
+    public static void main(String... args){
+        evaluateStream();
+        collectStream();
+        modifySteam();
+    }
 
     void createStream() {
 
@@ -34,54 +48,67 @@ public class Streams {
         IntStream is2 = "abc".chars();
     }
 
+    static void evaluateStream() {
+        integerStream = integerList.stream();
+        integerStream.allMatch(x -> x > 0);
 
-    void collectStream() {
 
-        Stream<Integer> intStream = Stream.of(1, 2, 3, 4);
-        List<Integer> intList = intStream.collect(Collectors.toList());
-
-        System.out.println(intList); //prints [1, 2, 3, 4]
-
-        intStream = Stream.of(1, 2, 3, 4); //stream is closed, so we need to create it again
-        Map<Integer, Integer> intMap = intStream.collect(Collectors.toMap(i -> i, i -> i + 10));
-        System.out.println(intMap); //prints {1=11, 2=12, 3=13, 4=14}
 
     }
 
 
-    void modifySteam() {
+    static void collectStream() {
 
-        List<Integer> myList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) myList.add(i);
-        Stream<Integer> sequentialStream = myList.stream();
+        integerList = integerList.stream().collect(Collectors.toList());  // [1, 2, 3, 4]
 
-        Stream<Integer> highNums = sequentialStream.filter(p -> p > 90); //filter numbers greater than 90
-        System.out.print("High Nums greater than 90=");
-        highNums.forEach(p -> System.out.print(p + " "));
-//prints "High Nums greater than 90=91 92 93 94 95 96 97 98 99 "
+        Map<Integer, Integer> intMap = integerList.stream().collect(Collectors.toMap(i -> i, i -> i + 10)); // {1=11, 2=12, 3=13, 4=14}
 
+        integerList.stream().forEach(p -> System.out.print(p + " "));
 
-        Stream<String> names = Stream.of("aBc", "d", "ef");
-        System.out.println(names.map(s -> {
-            return s.toUpperCase();
-        }).collect(Collectors.toList()));
-//prints [ABC, D, EF]
+        // consumes the stream, just like forEach - however is intermediate operation, thus not executed unless terminal operation present.
 
+        integerList.stream().map(x -> { System.out.println("foo"); return  2* x;});
+        integerStream = integerList.stream().map(x -> { System.out.println("foo"); return  2* x;});
+        integerList.stream().map(x -> { System.out.println("foo"); return  2* x;}).collect(Collectors.toList());
 
-        Stream<String> names2 = Stream.of("aBc", "d", "ef", "123456");
-        List<String> reverseSorted = names2.sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        System.out.println(reverseSorted); // [ef, d, aBc, 123456]
-
-        Stream<String> names3 = Stream.of("aBc", "d", "ef", "123456");
-        List<String> naturalSorted = names3.sorted().collect(Collectors.toList());
-        System.out.println(naturalSorted); //[123456, aBc, d, ef]
+        integerList.stream().peek(System.out::println);
+        integerList.stream().peek(System.out::println).collect(Collectors.toList());
+        integerList.stream().peek(System.out::println);
+    }
 
 
-        Stream<List<String>> namesOriginalList = Stream.of(Arrays.asList("Pankaj"), Arrays.asList("David", "Lisa"), Arrays.asList("Amit"));
-//flat the stream from List<String> to String stream
-        Stream<String> flatStream = namesOriginalList.flatMap(strList -> strList.stream());
 
-        flatStream.forEach(System.out::println);
 
+
+    static void modifySteam() {
+
+        integerStream = integerList.stream();
+        integerStream = integerStream.filter(p -> p > 7); // filter numbers greater than 90
+
+        // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+        integerStream = integerStream.map(x -> x +1);
+
+
+        integerStream = integerStream.sorted();
+        integerStream = integerStream.sorted(Comparator.reverseOrder());
+        integerStream = integerStream.sorted((x,y) -> x - y );
+
+
+        List<List<Integer>> bumpyList = new ArrayList<>();
+        bumpyList.add(integerList);
+        bumpyList.add(integerList);
+
+        // <R> Stream<R> flatMap(Function<? super T,? extends Stream<? extends R>> mapper)
+        // function transfers elements of collection to a temp_stream.
+        // afterwards all the temp_streams are concatenated and returned.
+        bumpyList.stream().flatMap(list -> list.stream()).forEach(System.out::println);
+        bumpyList.stream().flatMap(Collection::stream).forEach(System.out::println);
+        bumpyList.stream().flatMap(list -> list.stream().filter(x -> x > 3)).forEach(System.out::println);
+
+        // T reduce(T identity, BinaryOperator<T> accumulator);
+        // int reduce(int identity, IntBinaryOperator op);
+        // identity / initial value
+        integerList.stream().reduce(0,(a,b) -> a + b);
+        integerList.stream().reduce(0, Integer::sum);
     }
 }
