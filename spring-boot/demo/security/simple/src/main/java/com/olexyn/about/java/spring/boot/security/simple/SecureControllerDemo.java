4dd9718e5ce2e99c83c10eaded075f8a335204a8
io42630 <ivan@olexyn.com>
2022-05-30 14:25:35 +0200

    + add secu simple demo

---
+++ spring-boot/demo/security/simple/src/main/java/com/olexyn/about/java/spring/boot/security/simple/SecureControllerDemo.java
@@ -0,0 +1,12 @@
+package com.olexyn.about.java.spring.boot.security.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureControllerDemo {
+
+	public static void main(String... args) {
+		SpringApplication.run(SecureControllerDemo.class, args);
+	}
+}
