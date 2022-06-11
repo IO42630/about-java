9431035fe738697044dd63b43867c420e6411b20
io42630 <ivan@olexyn.com>
2022-06-11 15:36:27 +0200

    + revisit jpa

--- spring/jpa-boot/src/main/java/com/olexyn/java/about/spring/boot/jpa/FruitRepoJava.java
+++ spring/jpa-boot/src/main/java/com/olexyn/java/about/spring/boot/jpa/FruitRepoJava.java
@@ -2,6 +2,7 @@ package com.olexyn.java.about.spring.boot.jpa;
 
 import com.olexyn.java.about.spring.boot.jpa.model.FruitEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,9 @@ public interface FruitRepoJava extends JpaRepository<FruitEntity, Long> {
 
 
     List<FruitEntity> findByName(String name);
+
+    @Query("select x from FruitEntity x")
+    List<FruitEntity> findAllQuery();
+
+
 }
